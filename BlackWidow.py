36c86434c5c8ea48f7fc1a5fde0a1db286ab608e
Noah Hunt <noahhuntcte@gmail.com>
2025-02-20 14:06:00 -0500

    Added Libraries & While Loop for ellipsis - stable

--- BlackWidow.py
+++ BlackWidow.py
@@ -1,6 +1,21 @@
-import random
-from time import sleep
+#Libraries HERE
+import sys
+import time
 
-print("\n**************************************\n")
 print("Welcome Branch - Developer: Noah Hunt\n")
 
+print("\nWelcome to InfoTechCenter V1.0")
+
+x = 0
+ellipsis = 0
+
+while x != 20:
+    x += 1
+    message = ("Infotech Center System Booting" + "." * ellipsis)
+    ellipsis += 1
+    sys.stdout.write("\r" + message)
+    time.sleep(.5)
+    if ellipsis ==4:
+        ellipsis = 0
+    if x == 20:
+        print("\nOperating System Booted Up - Retina Scanned - Access Granted")
